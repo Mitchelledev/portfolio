ce3552ef236c308668d51d15191ab1601c03aed0
unknown <udohmichelle3@gmail.com>
2026-07-12 11:46:59 -0700

    add dark mode toggle and experience section

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function toggleMenu(){
     const navLinks = document.querySelector(".nav-links");
     navLinks.classList.toggle("open");
 }
+function toggleDarkMode(){
+    document.body.classList.toggle("dark-mode");
+}
